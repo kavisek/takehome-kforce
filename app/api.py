4cf38f3ba8c5658258a57ea12823b2e22057e72b
kavisek <kavi.skhon@gmail.com>
2022-05-24 01:24:44 +0100

    feat: update doc strings

--- app/api.py
+++ app/api.py
@@ -7,31 +7,41 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-def consolidated_dataframe():
-    """Read consolidate data"""
+def consolidated_dataframe() -> pd.DataFrame:
+    """Read consolidate dataframe.
+
+    Returns:
+        pd.DataFrame: consolidated dataframe.
+    """
     df = pd.read_csv("./output/consolidated_output.1.csv")
     return df
 
 
 @app.get("/")
-def read_root():
-    """Root: http://127.0.0.1:8000/"""
+def read_root() -> dict:
+    """Return full consolidated data
+
+    Request: http://127.0.0.1:8000/
+
+    Returns:
+        _type_: dict
+    """
     df = consolidated_dataframe()
     json = df.to_json()
     return json
 
 
 @app.get("/quality/{quality_id}")
-def read_quality(quality_id: str):
+def read_quality(quality_id: str) -> dict:
     """Return quality
 
     Request: http://127.0.0.1:8000/quality/low
 
     Args:
-        quality_id (str): _description_
+        quality_id (str): the quality id
 
     Returns:
-        _type_: _description_
+        _type_: dict
     """
     df = consolidated_dataframe()
     df = df[df["quality"] == quality_id]
